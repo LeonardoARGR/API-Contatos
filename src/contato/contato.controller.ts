fe25893fdedd217db79982107140f2bbaa4e3f22
LeonardoARGR <leoaragaogris@outlook.com>
2025-06-08 19:48:40 -0300

    finalizando o projeto criando o crud

--- src/contato/contato.controller.ts
+++ src/contato/contato.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { ContatoService } from './contato.service';
+import {Contato} from './contato.entity';
 
 @Controller('contato')
 export class ContatoController {
     constructor(private readonly contatoService: ContatoService) {}
+    
+    @Post()
+    criar(@Body() dados: Partial<Contato>) {
+        return this.contatoService.criar(dados);
+    }
+
     @Get()
-    findAll() {
-        return this.contatoService.findAll();
+    listar() {
+        return this.contatoService.listarTodos();
+    }
+
+
+    @Get(':id')
+    buscar(@Param('id') id:string) {
+        return this.contatoService.buscarPorId(id);
+    }
+
+    @Put(':id')
+    atualizar(@Param('id') id: string, @Body() dados: Partial<Contato>) {
+        return this.contatoService.atualizar(id, dados);
+    }
+
+    @Delete(':id')
+    remover(@Param('id') id: string) {
+        return this.contatoService.deletar(id);
     }
 }
--- src/contato/contato.service.ts
+++ src/contato/contato.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
+import { ObjectId } from 'mongodb';
 import { Contato } from './contato.entity';
 
 @Injectable()
@@ -10,7 +11,32 @@ export class ContatoService {
         private contatoRepository: MongoRepository<Contato>,
     ) {}
     
-    findAll(): Promise<Contato[]> {
+    async criar(contato: Partial<Contato>) {
+        const novo = this.contatoRepository.create(contato);
+        return this.contatoRepository.save(novo);
+    }
+
+    async listarTodos() {
         return this.contatoRepository.find();
+    }
+
+    async buscarPorId(id: string) {
+        return this.contatoRepository.findOne({
+            where: { _id: new ObjectId(id) }
+        });
+    }
+
+    async deletar(id: string): Promise<void> {
+        await this.contatoRepository.delete({ id: new ObjectId(id) });      
+    }
+
+    async atualizar(id: string, dados: Partial<Contato>) {
+        const contato = await this.buscarPorId(id);
+        if (!contato) {
+            throw new NotFoundException('Contato não encontrado');
+        }
+
+        const atualizado = Object.assign(contato, dados);
+        return this.contatoRepository.save(atualizado);
     }
 }
